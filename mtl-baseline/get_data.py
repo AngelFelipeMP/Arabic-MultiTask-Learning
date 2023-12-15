1180a7fa84a70a5798fbf77dd8ff3b7b37a9c463
AngelFelipeMP <angel.magnossao@gmail.com>
2023-12-15 06:49:04 +0100

    include func to delet old processed datasets

--- mtl-baseline/get_data.py
+++ mtl-baseline/get_data.py
@@ -1,10 +1,31 @@
 import os
 import sys
+import shutil
 sys.path.append(os.getcwd().split('Arabic-MultiTask-Learning')[0] + 'Arabic-MultiTask-Learning')
 from data_exploration import ArMI2021, OSACT2022, HSARABIC
 from config import *
 
+def clear_directory(dir_path):
+    # Check if the directory exists
+    if os.path.exists(dir_path) and os.path.isdir(dir_path):
+        # Iterate over all files in the directory
+        for filename in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)  # Remove file or link
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)  # Remove subdirectories and files within
+            except Exception as e:
+                print(f'Failed to delete {file_path}. Reason: {e}')
+    else:
+        print(f"The directory {dir_path} does not exist.")
+
 if __name__ == '__main__':
+    #remove old data
+    clear_directory(BASELINE_DATA_PATH)
+    
+    #process data
     ArMI_2021 = ArMI2021()
     ArMI_2021.save_tsv(
                 path=BASELINE_DATA_PATH,
